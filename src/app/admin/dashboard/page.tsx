90f615a79a313e71f3278927e699b738dfad4535
Manish Kumar <kmanish57610@gmail.com>
2025-11-25 16:23:05 +0530

    Made admin dashboard and used caching to fetch movies

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -62,25 +62,4 @@ export default async function AdminDashboard() {
   );
 }
 
-export function ActionCard({
-  title,
-  desc,
-  href,
-}: {
-  title: string;
-  desc: string;
-  href: string;
-}) {
-  return (
-    <div className="bg-white shadow-sm rounded-xl p-5 border hover:shadow-lg transition-all duration-200">
-      <h2 className="text-lg font-semibold text-gray-800 mb-2">{title}</h2>
-      <p className="text-sm text-gray-500 mb-4">{desc}</p>
-      <Link
-        href={href}
-        className="inline-block bg-red-600 text-white px-4 py-2 rounded-lg text-sm hover:bg-red-700 transition"
-      >
-        Manage
-      </Link>
-    </div>
-  );
-}
+
